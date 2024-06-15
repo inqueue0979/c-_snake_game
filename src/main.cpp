e1063012714891744ca6db5dc6af9004fa7996cc
inqueue0979 <inqueue0979@gmail.com>
2024-06-15 23:10:48 +0900

    Made Changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 #include "SnakeMap.h"
 #include "MapData.h"
 #include <locale.h>
+#include <iostream>
+using namespace std;
 
 int main() {
     setlocale(LC_ALL, "");
-    if(LINES < 42 || COLS < 84) {
-        initscr();
-        printw("LARGER TERMINAL REQUIRED\n42x84 LEAST");
+
+    initscr();
+    int max_y, max_x;
+    getmaxyx(stdscr, max_y, max_x);
+
+    getmaxyx(stdscr, max_y, max_x);
+
+    if(max_y < 42 || max_x < 84) {
+        mvwprintw(stdscr, 1, 0, "Current Screen Size | max_y: %d, max_x: %d", max_y, max_x);
+        printw("\n\nLARGER TERMINAL REQUIRED\n42x84 LEAST");
         refresh();
         getch();
         endwin();
