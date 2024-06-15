aff37608314f095ff1657e6d10585eee683ad317
inqueue0979 <inqueue0979@gmail.com>
2024-06-15 16:55:34 +0900

    Added MapData, Starts game when press Start

--- include/MapData.h
+++ include/MapData.h
@@ -1,9 +1,9 @@
 #ifndef MAPDATA_H
 #define MAPDATA_H
 
-extern int level_1[21][21];
-extern int level_2[21][21];
-extern int level_3[21][21];
-extern int level_4[21][21];
+extern int level_1[25][25];
+extern int level_2[25][25];
+extern int level_3[25][25];
+extern int level_4[25][25];
 
 #endif
--- src/SnakeMap.cpp
+++ src/SnakeMap.cpp
@@ -1,41 +1,57 @@
 #include "SnakeMap.h"
-#include <ncurses.h>
+#include <iostream>
 
 SnakeMap::SnakeMap(int size) : size(size) {
+    // 2D 배열 동적 할당
     map = new int*[size];
     for (int i = 0; i < size; ++i) {
         map[i] = new int[size];
     }
+
+    // ncurses 초기화 및 윈도우 생성
+    initscr();
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    mapWin = newwin(42, 84, (LINES - 42) / 2, (COLS - 84) / 2);
+    wbkgd(mapWin, COLOR_PAIR(1));
+    box(mapWin, 0, 0);
+    wrefresh(mapWin);
 }
 
 SnakeMap::~SnakeMap() {
+    // 동적 할당 해제
     for (int i = 0; i < size; ++i) {
         delete[] map[i];
     }
     delete[] map;
+    // 윈도우 종료
+    delwin(mapWin);
+    endwin();
 }
 
-void SnakeMap::loadMap(int level[21][21]) {
+void SnakeMap::loadMap(int mapData[25][25]) {
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            map[i][j] = level[i][j];
+            map[i][j] = mapData[i][j];
         }
     }
 }
 
 void SnakeMap::drawMap() {
+    // 테두리 그리기
+    box(mapWin, 0, 0);
+
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
             if (map[i][j] == 1) {
-                mvaddch(i, j, '#'); // 벽
-            } else if (map[i][j] == 0) {
-                mvaddch(i, j, ' '); // 빈 공간
+                wattron(mapWin, COLOR_PAIR(2));
+                mvwprintw(mapWin, i + 1, j * 2 + 1, "#"); // 가로로 한 칸씩 띄워서 출력
+                wattroff(mapWin, COLOR_PAIR(2));
+            } else {
+                mvwprintw(mapWin, i + 1, j * 2 + 1, " "); // 가로로 한 칸씩 띄워서 출력
             }
         }
     }
-    refresh();
-}
-
-bool SnakeMap::isCollision(int x, int y) {
-    return map[y][x] == 1;
+    wrefresh(mapWin);
 }
