f7150a71b7ddc5bb1fccba2e8a1249cc03afd269
joseokbin0125 <jingjingoh25@gmail.com>
2024-06-15 21:35:53 +0900

    Add files via upload

---
+++ include/item.h
@@ -0,0 +1,21 @@
+#include "Position.h"
+
+// 아이템 속성에 대응하는 상수값 정의
+#define POISON_ITEM 5
+#define GROW_ITEM   6
+
+// 아이템을 표기할 유니코드값 정의
+#define POISON_CHAR 0x2b14
+#define GROW_CHAR   0x2b15
+
+class Item {
+private:
+    Position itemPosition; // 아이템이 위치한 위치
+    int age; // 아이템이 맵에 출현한 후 지난 시간
+public:
+    int getAge();
+    void setAge(int a);
+    Position* getItemPosition();
+    Item(int y, int x, int type);
+    Item();
+};
