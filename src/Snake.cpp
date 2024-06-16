a939f1a6476621053cbbf743a2d7cc8f0d47490a
inqueue0979 <inqueue0979@gmail.com>
2024-06-16 22:04:04 +0900

    Made Length Death Precise

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -7,7 +7,6 @@ Snake::Snake(int startX, int startY, SnakeMap& snakeMap, ScoreBoard& scoreBoard)
     : snakeMap(snakeMap), scoreBoard(scoreBoard), currentDirection(RIGHT) {
     reset(startX, startY); // 초기화 로직을 reset 메서드로 이동
     gameOver = false;
-    snakeLength = 3;
 }
 
 void Snake::reset(int startX, int startY) {
@@ -78,7 +77,6 @@ void Snake::move(Gate& gateManager) {
         case 5: // Growth item
             addBodySegment();
             snakeMap.setMap(nextY, nextX, 0);
-            snakeLength++;
             scoreBoard.addScore(20);
             scoreBoard.addGrowthEaten(1);
             scoreBoard.addBodyCurrentLength(1);
@@ -89,12 +87,11 @@ void Snake::move(Gate& gateManager) {
         case 6: // Poison item
             removeBodySegment();
             snakeMap.setMap(nextY, nextX, 0);
-            snakeLength--;
             scoreBoard.addScore(-10);
             scoreBoard.addPoisonEaten(1);
             scoreBoard.addBodyCurrentLength(-1);
 
-            if(snakeLength < 3)
+            if(scoreBoard.getBodyCurrentLength() < 3)
                 gameOver = true;
             break;
         case 7: // Gate
