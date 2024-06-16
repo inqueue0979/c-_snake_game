f0b5e872e87158c257f694014a30966c3aacf888
inqueue0979 <inqueue0979@gmail.com>
2024-06-16 15:17:41 +0900

    Made subscreen Global

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "MainMenu.h"
 #include "SnakeMap.h"
 #include "MapData.h"
+#include "ScoreBoard.h"
 #include <locale.h>
 #include <iostream>
 using namespace std;
@@ -12,8 +13,6 @@ int main() {
     int max_y, max_x;
     getmaxyx(stdscr, max_y, max_x);
 
-    getmaxyx(stdscr, max_y, max_x);
-
     if(max_y < 42 || max_x < 84) {
         mvwprintw(stdscr, 1, 0, "Current Screen Size | max_y: %d, max_x: %d", max_y, max_x);
         printw("\n\nLARGER TERMINAL REQUIRED\n42x84 LEAST");
@@ -23,14 +22,26 @@ int main() {
         return 0;
     }
 
+    WINDOW* mainWin = newwin(42, 84, (LINES - 42) / 2, (COLS - 84) / 2);
+    wbkgd(mainWin, COLOR_PAIR(1));
+    box(mainWin, 0, 0);
+    wrefresh(mainWin);
+
     MainMenu menu;
-    SnakeMap snakeMap(25);
+    SnakeMap snakeMap(25, mainWin);
     snakeMap.loadMap(level_test);
+
+    ScoreBoard scoreBoard(mainWin, 28, 24, 54, 12); // 오른쪽에 위치하도록 설정 (start_x=54, start_y=12)
     int choice = menu.display();
 
     switch (choice) {
         case 1: // 게임 시작
-            snakeMap.drawMap(3, 1);
+            while(true)
+            {
+                snakeMap.drawMap(3, 1);
+                scoreBoard.display();
+                getch();
+            }
             getch();
             endwin();
             break;
