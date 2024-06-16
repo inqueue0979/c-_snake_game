643c49cd35419257f5fd6466adf06213e6010639
jooya38 <107492177+jooya38@users.noreply.github.com>
2024-06-16 14:35:16 +0900

    Update Snake.h

--- include/Snake.h
+++ include/Snake.h
@@ -3,18 +3,36 @@
 
 #include <deque>
 #include <utility>
+#include "SnakeMap.h"
+#include "InputHandler.h"
+#include "MapData.h"
+
+enum Direction { UP, DOWN, LEFT, RIGHT };
 
 class Snake {
 public:
-    Snake(int startX, int startY);
-    void move(char direction);
+    Snake(int startX, int startY, SnakeMap& snakeMap);
+    void changeDirection(Direction newDirection);
+    void move();
     void grow();
-    bool checkCollision(int mapWidth, int mapHeight);
-    const std::deque<std::pair<int, int> >& getBody() const;
+    bool checkSnakeCollision();
+    void render();
+    void gameLoop();
+
+    std::deque<std::pair<int, int>> getBody() const;
 
 private:
-    std::deque<std::pair<int, int> > body;
-    char currentDirection;
+    std::deque<std::pair<int, int>> body;
+    Direction currentDirection;
+    InputHandler inputHandler;
+    int tick;
+    int growthCount;
+    SnakeMap& snakeMap;
+
+    void checkMapCollision();
+    void determineGateExit(int gateY, int gateX);
+    bool isValidMove(int nextX, int nextY);
+    bool isCollision(int x, int y);
 };
 
 #endif // SNAKE_H
